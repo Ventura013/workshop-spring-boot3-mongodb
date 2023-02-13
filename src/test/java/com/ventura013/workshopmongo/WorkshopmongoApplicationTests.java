10c5d8323fd826dd35d4aa60ddf00c0fd9445875
Diego Ventura <diego.venturaa89@gmail.com>
2023-02-13 19:04:59 -0300

    project created

---
+++ src/test/java/com/ventura013/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventura013.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
