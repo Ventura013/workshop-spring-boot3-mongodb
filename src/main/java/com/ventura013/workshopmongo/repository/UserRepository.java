2f4b112f1c1e1542954d77243d7fdf568f2b1c81
Diego Ventura <diego.venturaa89@gmail.com>
2023-02-14 13:17:49 -0300

    Using DTO pattern to return users

--- src/main/java/com/ventura013/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/ventura013/workshopmongo/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.ventura013.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.ventura013.workshopmongo.domain.User;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
 }
