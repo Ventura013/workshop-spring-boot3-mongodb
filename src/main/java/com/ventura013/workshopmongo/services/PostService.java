681f24ae636f2cb3e062ff5a7f209e7d24e36759
Diego Ventura <diego.venturaa89@gmail.com>
2023-02-14 22:04:20 -0300

    Getting a post by id

---
+++ src/main/java/com/ventura013/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.ventura013.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ventura013.workshopmongo.domain.Post;
+import com.ventura013.workshopmongo.repository.PostRepository;
+import com.ventura013.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	public PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("object not found"));
+		}
+	
+}
