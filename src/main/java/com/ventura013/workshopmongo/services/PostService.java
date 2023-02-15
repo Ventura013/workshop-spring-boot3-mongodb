2e29c3095f47cdfe0afb88a55bb2d9175bb92a7f
Diego Ventura <diego.venturaa89@gmail.com>
2023-02-15 12:22:44 -0300

    Simple query with @Query

--- src/main/java/com/ventura013/workshopmongo/services/PostService.java
+++ src/main/java/com/ventura013/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ventura013.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("object not found"));
 		}
 	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
